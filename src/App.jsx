6d93c544ef801b1d69127100628eba3827401bc3
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-10-20 14:13:06 -0700

    my new portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
-import React from "react"
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import AboutMe from './components/AboutMe'
-import Experience from './components/Experience'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Contact from './components/Contact'
-import Footer from "./components/Footer"
 import './App.css'
+import AppRoutes from "./routes"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
-
-  let component
-  switch (window.location.pathname) {
-    case '/':
-      component = <Home />
-      break;
-    case '/aboutMe':
-      component = <AboutMe />
-      break;
-    case '/experience':
-      component = <Experience />
-      break;
-    case '/projects':
-      component = <Projects />
-      break;
-    case '/skills':
-      component = <Skills />
-      break;
-    case '/contact':
-      component = <Contact />
-      break;
-  }
-
   return (
     <div>
       <Navbar />
       <div className='main'>
         <div className='container'>
           <div className='obj-comp'>
-            {component}
+            <AppRoutes />
           </div>
         </div>
       </div>
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import home from "../../components/database/home"
+import Paragraph from "../../components/subcomponents/Paragraph"
+
+function Home() {
+
+  const homeParagraph = home.map(item => {
+    return(
+            <Paragraph 
+                key={item.id}
+                value={item.value}
+            />
+    )
+  })
+  return (
+    <section>
+        <div className="dev-img-div">
+            <img src="./src/images/developer.png" className="dev-img" />
+            <h1 className="dev-text">Welcome</h1>
+        </div>
+        <h1 className="comp-subpar">Home</h1>
+        <div className="center-photo">
+            <img src="./src/images/wagner.png" className="photo-wagner" />
+        </div>
+        <div>
+            {homeParagraph}
+        </div>
+    </section>
+  )
+}
+
+export default Home
