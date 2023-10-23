d5710dcb9270828400d27a95251073cb8b18a43c
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-10-23 14:15:31 -0700

    new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import AppRoutes from "./routes"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
 
 export default function AppRoutes(){
     return(
-        <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <Home /> } ></Route>
-                <Route path="/" element={ <AboutMe /> } ></Route>
+                <Route path="/AboutMe" element={ <AboutMe /> } ></Route>
+                <Route path="/Experience" element={ <Experience /> } ></Route>
+                <Route path="/Projects" element={ <Projects /> } ></Route>
+                <Route path="/Skills" element={ <Skills /> } ></Route>
+                <Route path="/Contact" element={ <Contact /> } ></Route>
             </Routes>
-        </BrowserRouter>
     )
 }
